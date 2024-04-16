1c746fbc74ac4236926dd9c2f62d580045a4788c
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-04-16 12:21:21 +0100

    Refactor card slider component in App.js and update README.md

--- card-slider/README.md
+++ card-slider/README.md
@@ -3,3 +3,11 @@ Card slider for react projects
 ALWAYS CD INTO THE APP FOLDER BEFORE EDITING
 
 cd name-of-project
+
+To get the app working we need to install tailwind using the npm command
+
+React slick is another tool that will be used to form the slideing effect
+https://react-slick.neostack.com/docs/get-started
+npm install react-slick --save
+follow the rest of the page
+
--- card-slider/src/App.js
+++ card-slider/src/App.js
@@ -5,15 +5,15 @@ function App() {
     <div className="w-3/4 m-auto">
       <div className='mt-20'>
         {data.map((d) => (
-          <div>
-            <div>
-              <img src={d.img} alt="" />
+          <div className="bg-white h-[450px] text-black rounded-xl">
+            <div className="h-56 rounded-t-xl bg-green-200 flex justify-center items-center">
+              <img src={d.img} alt="" className="h-40 w-40 rounded-full"/>
             </div>
 
-            <div>
-              <p>{d.name}</p>
-              <p>{d.job}</p>
-              <button>View Profile</button>
+            <div className="flex flex-col justify-center items-center gap-2 p-3">
+              <p className="text-xl font-semibold">{d.name}</p>
+              <p className="">{d.job}</p>
+              <button className="bg-green-300 text-white text-lg px-6 py-1 rounded-xl">View Profile</button>
             </div>
           </div>
         ))}
@@ -29,28 +29,28 @@ function App() {
 const data = [
   {
   name: "John Doe",
-  img: "../public/users/user1.jpg",
+  img: "/users/user1.jpg",
   job: "Software Engineer",
   },
   {
-  name: "John Doe",
+  name: "John Smith",
   img: "/users/user2.jpg",
-  job: "Software Engineer",
+  job: "Software Developer",
   },
   {
-  name: "John Doe",
-  img: "./public/users/user3.jpg",
-  job: "Software Engineer",
+  name: "Jane Doe",
+  img: "/users/user3.jpg",
+  job: "Graphic Designer",
   },
   {
-  name: "John Doe",
-  img: "./public/users/user4.jpg",
-  job: "Software Engineer",
+  name: "Sven Doe",
+  img: "/users/user4.jpg",
+  job: "Product Manager",
   },
   {
-  name: "John Doe",
-  img: "./public/users/5.jpg",
-  job: "Software Engineer",
+  name: "Sam Doe",
+  img: "/users/user5.jpg",
+  job: "UI/UX Designer",
   },
 ]
 
