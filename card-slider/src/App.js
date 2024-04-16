8f04b52ef238bee6026728124f15370d6612e132
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-04-16 11:59:34 +0100

    Refactor App.js to include a card slider component and add Tailwind CSS imports

--- card-slider/src/App.js
+++ card-slider/src/App.js
@@ -2,10 +2,57 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    <h1 className="text-7xl text-center text-blue-400" >Test Card Slider</h1>
+    <div className="w-3/4 m-auto">
+      <div className='mt-20'>
+        {data.map((d) => (
+          <div>
+            <div>
+              <img src={d.img} alt="" />
+            </div>
+
+            <div>
+              <p>{d.name}</p>
+              <p>{d.job}</p>
+              <button>View Profile</button>
+            </div>
+          </div>
+        ))}
+
+      </div>
+    
     </div>
   );
 }
 
+
+
+const data = [
+  {
+  name: "John Doe",
+  img: "../public/users/user1.jpg",
+  job: "Software Engineer",
+  },
+  {
+  name: "John Doe",
+  img: "/users/user2.jpg",
+  job: "Software Engineer",
+  },
+  {
+  name: "John Doe",
+  img: "./public/users/user3.jpg",
+  job: "Software Engineer",
+  },
+  {
+  name: "John Doe",
+  img: "./public/users/user4.jpg",
+  job: "Software Engineer",
+  },
+  {
+  name: "John Doe",
+  img: "./public/users/5.jpg",
+  job: "Software Engineer",
+  },
+]
+
 export default App;
+
